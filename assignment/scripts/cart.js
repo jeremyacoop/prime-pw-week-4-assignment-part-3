2294ee5b41586971766f03b94ec7566319cf12ba
jeremyacoop <63517273+jeremyacoop@users.noreply.github.com>
2021-11-21 15:05:11 -0600

    Wrote required functions

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,31 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+
+function addItem(item) {
+    basket.push(item);
+    return true;
+}// end addItem
+
+function listItems(basket) {
+    for (let i=0; i<basket.length; i++) {
+        console.log(basket[i]);
+    }
+}// end listItems
+
+function empty(basket) {
+    basket = [];
+
+}// end empty
+
+console.log(addItem('broccoli'));
+console.log(addItem('asparagus'));
+console.log(addItem('kumquats'));
+console.log(addItem('grapes'));
+
+console.log(basket);
+listItems(basket);
+
+empty(basket);
+console.log(basket);
